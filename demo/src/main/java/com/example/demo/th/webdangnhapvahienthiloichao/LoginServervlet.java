3fc1831e98e8bc3081424ce4b539d0c9ebfb1a52
Dungndph20697 <dungthoai321@gmail.com>
2025-08-28 08:40:24 +0700

    Tạo trang web để đăng nhập và hiển thị lời chào

---
+++ demo/src/main/java/com/example/demo/th/webdangnhapvahienthiloichao/LoginServervlet.java
@@ -0,0 +1,32 @@
+package com.example.demo.th.webdangnhapvahienthiloichao;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name = "LoginServlet", value = "/webdangnhapvahienthiloichao/login")
+public class LoginServervlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/webdangnhapvahienthiloichao/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        if ("admin".equals(username) && "admin".equals(password)) {
+            writer.println("<h1>Welcome " + username + " to website</h1>");
+        } else {
+            writer.println("<h1>Login Error</h1>");
+        }
+        writer.println("</html>");
+    }
+}
